eb3e45b40212411d9da258455ca13589cbcd1510
Mahizhan7020 <samahizhan2008@gmail.com>
2026-03-17 12:59:17 +0530

    UC3 was implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, Guest!");
-        }
+        // If no argument is provided, default to "World"
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello " + name + "!");
     }
 }
