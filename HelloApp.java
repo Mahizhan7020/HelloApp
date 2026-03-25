4a340fcbe2a538fc00b8375abcee4cea5e6df4be
Mahizhan7020 <samahizhan2008@gmail.com>
2026-03-25 20:27:27 +0530

    UC7 was implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,18 +1,13 @@
-public class HelloArguments {
+public class HelloArgumentsJoin {
     public static void main(String[] args) {
-       
+        
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder greeting = new StringBuilder("Hello, ");
             
-            
-            for (String name : args) {
-                greeting.append(name).append(", ");
-            }
-            String finalGreeting = greeting.substring(0, greeting.length() - 2);
-            
-            System.out.println(finalGreeting);
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names);
         }
     }
 }
+
